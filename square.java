1b07f93435fe77ad03440c649388ed2a9514976c
Peter Stenger <407869@HURimcL130206bd.local>
2019-02-04 14:39:28 -0500

    save

--- square.java
+++ square.java
@@ -3,7 +3,7 @@ import java.awt.GridLayout;
 import java.io.*;
 import java.util.Scanner;
 import java.util.function.BiPredicate;
-
+import java.awt.image.*;
 import javax.swing.*;
 import javax.imageio.ImageIO;
 import java.io.File;
@@ -12,10 +12,14 @@ public class square extends JPanel {
     square(int i, int j) {
         System.out.println("cell initilized" + i);
         this.setSize(64,64);
-        JButton button = new JButton();
-        Image img = ImageIO.read(getClass().getResource("assets/black-piece.png"));
-        button.setIcon(new ImageIcon(img));
-        this.add(button);
+        BufferedImage img = null;
+        try {
+            img = ImageIO.read(this.getClass().getResource("assets/black-piece.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        JLabel text = new JLabel(new ImageIcon(img));
+        this.add(text);
         this.setVisible(true);
     }
 }
